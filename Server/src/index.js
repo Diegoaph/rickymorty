3622a8d1fd76a504578a3d4d409c188d295bfc5f
Diegoaph <Diegoaph2018@gmail.com>
2023-08-04 15:47:06 -0500

    publish2

--- Server/src/index.js
+++ Server/src/index.js
@@ -9,7 +9,7 @@ server.use(express.json());
 server.use((req, res, next) => {
     res.header(
         "Access-Control-Allow-Origin",
-        "https://devdiego-rickymorty-back.up.railway.app/"
+        "https://devdiego-rickymorty.vercel.app"
     );
     res.header("Access-Control-Allow-Credentials", "true");
     res.header(
